328dfe187ee6f270b8556f139be1aeca2237a9d7
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-07-17 10:10:57 +0700

    commit

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,5 @@
 export const errorMiddleware = (error, req, res, next) => {
-    const status = error.status || 500
+    const status = error.status || 404
     const message = error.message || 'Something went wrong'
     res
         .status(status)
