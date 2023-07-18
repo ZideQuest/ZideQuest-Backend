e1aa7497ac8447a0e81827ee4ee5be4b1d477adc
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-07-18 08:52:27 +0700

    schema for  quest

---
+++ model/quest.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const questSchema = new mongoose.Schema({
+    questName: {
+        type: String,
+        require: true,
+    },
+    adminId: {
+        type: mongoose.Types.ObjectId,
+        ref: "Admin",
+    },
+    locationId: {
+        type: mongoose.Types.ObjectId,
+        ref: "Location",
+    },
+    timeStart: {
+        type: Date,
+        require: true,
+    },
+    timeEnd: {
+        type: Date,
+        require: true,
+    },
+    description: {
+        type: string,
+    },
+    image: {
+        type: Array,
+    },
+    tagId: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Tag"
+    }],
+    questStatus: {
+        type: Boolean,
+        default: false
+    },
+    maxParticipant: {
+        type: Int16Array,
+    },
+    qrCode: {
+        type: Int16Array,
+    },
+    participant: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Participant",
+    }]
+}, {
+    timestamps: true
+})
+
+const Quest = mongoose.model("quest", questSchema)
+export default Quest
