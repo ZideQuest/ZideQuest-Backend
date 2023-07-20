1bf72de3e9c00225d1bb98d7675ed03440128e4d
RatchanonWeb <124414072+RatchanonWeb@users.noreply.github.com>
2023-07-21 00:27:13 +0700

    create location schema

---
+++ model/location.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const locationSchema = new mongoose.Schema({
+    locationName: {
+        type: String,
+        require: true,
+        unique: true,
+        trim: true,
+        maxlength: [50,'Your location name must be less than 50 characters'],
+        minlength: [2,'Your location name must be longer than 2 characters'],
+    },
+    location: {
+        type: {
+            type: String,
+            require: true,
+            enum: ["Point"], 
+        },
+        coordinate: {
+            type: [Number],
+            require: true,
+            unique: true,
+        }
+    },
+    locationPicturePath: {
+        type: String,
+        require: true,
+        unique: true,
+        trim: true,
+    },
+}, {
+    timestamps: true
+})
+
+const Location = mongoose.models.Location || mongoose.model("Location", tagSchema)
+export default Location
