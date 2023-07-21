acbd97475eccfeb39dd390ee99e96b44bcf41219
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-07-21 23:01:22 +0700

    add login to accounrSchema

--- model/account.js
+++ model/account.js
@@ -48,6 +48,25 @@ accountSchema.statics.createAccount = async function (username, password, role) 
         throw error
     }
 }
+/* LOGIN */
+accountSchema.statics.login = async function (username, password) {
+    try {
+        // validate
+        if (!(username && password)) throw new Error("All field is required")
+
+        // this account already exists
+        const account = await this.findOne({ username })
+        if (!account) throw new Error("Wrong username or password")
+
+        // check password if match => return account 
+        const match = await bcrypt.compare(password, account.password)
+        if (!match) throw new Error("Wrong username or password")
+
+        return account
+    } catch (error) {
+        throw error
+    }
+}
 
 const Account = mongoose.models.Account || mongoose.model("Account", accountSchema)
 
