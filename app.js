231919ffb706d97c5d068e79152ea91d9457a739
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-07-21 23:01:05 +0700

    add authRoutes

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ import cookieParser from "cookie-parser";
 import basicRoutes from './routes/basic-api.js'
 import accountRoutes from './routes/account.js'
 import userRoutes from './routes/user.js'
+import authRoutes from './routes/auth.js'
 import { errorMiddleware } from './middleware/errorHandler.js'
 
 
@@ -62,6 +63,7 @@ function createApp() {
     app.use("/api/basic-api", basicRoutes);
     app.use("/api/account", accountRoutes);
     app.use("/api/user", userRoutes);
+    app.use("/api/auth", authRoutes);
 
     // Error handler
     app.use(errorMiddleware);
