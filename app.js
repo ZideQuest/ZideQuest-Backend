81d2131bd83abfcda0d2aa5d1b6e42d1c883a99e
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-07-21 18:41:03 +0700

    add app.js and some package

--- app.js
+++ app.js
@@ -1,84 +1,69 @@
-const express = require("express");
-const morgan = require("morgan");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const hpp = require("hpp");
-const compression = require("compression");
-import { logger } from "./util/logger.js";
+// Package Import
+import express from 'express'
+import morgan from 'morgan';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
+import rateLimit from 'express-rate-limit'
+import helmet from 'helmet'
+import hpp from 'hpp'
+import compression from 'compression';
+import xXssProtection from 'x-xss-protection';
 import cookieParser from "cookie-parser";
 
-// const AppError = require("./utils/appError");
-// const errorController = require("./controllers/errorController");
-// const tourRouter = require("./routes/tourRoutes");
-// const userRouter = require("./routes/userRoutes");
-// const reviewRouter = require("./routes/reviewRoutes");
-
+// Routes Import
 import basicRoutes from './routes/basic-api.js'
 import accountRoutes from './routes/account.js'
 import { errorMiddleware } from './middleware/errorHandler.js'
 
-const app = express();
 
-// security middleware
-//set security for http headers
-app.use(helmet());
+function createApp() {
+    // Express app
+    const app = express();
 
-//limiting request
-const limiter = rateLimit({
-  max: 100,
-  wondowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour",
-});
-app.use("/api", limiter);
+    //limiting request
+    const limiter = rateLimit({
+        max: 100,
+        wondowMs: 60 * 60 * 1000,
+        message: "Too many requests from this IP, please try again in an hour",
+    });
 
-// just logging testing middleware
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-  app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-  });
+    //PUT FREQUENTLY USE WORD IN ARRAY BELOW KRUB
+    const hppH = hpp({
+        whitelist: [
+            // 'duration',
+            // 'ratingsQuantity',
+            // 'ratingsAverage',
+            // 'maxGroupSize',
+            // 'difficulty',
+            // 'price',
+        ],
+    })
+
+    // security middleware
+    app.use(hppH)
+    app.use("/api", limiter);
+    app.use(express.json({ limit: "10kb" })); //body parser, reading data from body into req.body
+    app.use(helmet());
+    app.use(cookieParser())             // Cookies
+    app.use(ExpressMongoSanitize());    // data sanitiztion against NoSQL query injection
+    app.use(xXssProtection());          // data sanitiztion against XSS
+    app.use(compression());             // compress requests
+
+    // just logging testing middleware
+    if (process.env.NODE_ENV === "development") {
+        app.use(morgan("dev"));
+        app.use((req, res, next) => {
+            req.requestTime = new Date().toISOString();
+            next();
+        });
+    }
+
+    // routes
+    app.use("/api/basic-api", basicRoutes);
+    app.use("/api/account", accountRoutes);
+
+    // Error handler
+    app.use(errorMiddleware);
+    return app
 }
 
-//body parser, reading data from body into req.body
-app.use(express.json({ limit: "10kb" }));
-app.use(cookieParser())
-
-//data sanitiztion against NoSQL query injection
-app.use(mongoSanitize());
-
-//data sanitiztion against XSS
-app.use(xss());
-
-//clear up parameter pollution
-//PUT FREQUENTLY USE WORD IN ARRAY BELOW KRUB
-app.use(
-  hpp({
-    whitelist: [
-      // 'duration',
-      // 'ratingsQuantity',
-      // 'ratingsAverage',
-      // 'maxGroupSize',
-      // 'difficulty',
-      // 'price',
-    ],
-  })
-);
-
-//compress requests
-app.use(compression());
-
-//routes
-app.use("/api/basic-api", basicRoutes);
-app.use("/api/account", accountRoutes);
-
-//error middleware routes
-// app.all("*", (req, res, next) => {
-//   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
-// });
-app.use(errorMiddleware);
-
-//start server
-module.exports = app;
+export default createApp
--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-
 import { logger } from './util/logger.js'
 import { connectDb } from './util/connectDb.js'
+import createApp from './app.js'
 
-import app from './app'
+function main() {
+    // dotenv
+    dotenv.config();
+    const app = createApp()
 
-// dotenv.config({ path: './config.env' });
-dotenv.config();
+    // define port and start server
+    const PORT = process.env.PORT || 3000;
+    const server = app.listen(PORT, () => {
+        logger.info(`server start at http://localhost:${PORT}`)
+        connectDb()
+    });
 
-//define port and start server
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
-  logger.info(`server start at http://localhost:${PORT}`)
-  connectDb()
-});
 
-process.on('unhandledRejection', (err) => {
-  console.error(err.name, err.message, '💥');
-  console.error('Shutting down...');
-  server.close(() => {
-    process.exit(1);
-  });
-});
+    process.on('unhandledRejection', (err) => {
+        console.error(err.name, err.message, '💥');
+        console.error('Shutting down...');
+        server.close(() => {
+            process.exit(1);
+        });
+    });
 
-process.on('uncaughtException', (err) => {
-  console.error(err.name, err.message, '💥');
-  console.error('Shutting down...');
-  server.close(() => {
-    process.exit(1);
-  });
-});
+    process.on('uncaughtException', (err) => {
+        console.error(err.name, err.message, '💥');
+        console.error('Shutting down...');
+        server.close(() => {
+            process.exit(1);
+        });
+    });
+
+}
+
+main()
