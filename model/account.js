81b860513937f416d22f5d227b9aa1b9532a5a63
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-07-21 19:52:15 +0700

    add createAccount statics

--- model/account.js
+++ model/account.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 
 const accountSchema = new mongoose.Schema({
     username: {
@@ -23,4 +24,31 @@ const accountSchema = new mongoose.Schema({
 })
 
 const Account = mongoose.models.Account || mongoose.model("Account", accountSchema)
+
+/* CREATE ACCOUNT */
+
+accountSchema.statics.createAccount = async function (username, password, role) {
+    try {
+        // validate
+        if (!(username && password && role)) throw new Error("All field is required")
+
+        // this account already exists
+        const account = await Account.find({ username })
+        if (account) throw new Error("This username already registered")
+
+        // hash password
+        const salt = await bcrypt.genSalt()
+        const hash = await bcrypt.hash(password, salt)
+
+        // create account
+        const newAccount = await Account.create({
+            username,
+            password: hash,
+            role,
+        })
+        return newAccount
+    } catch (error) {
+        throw error
+    }
+}
 export default Account
