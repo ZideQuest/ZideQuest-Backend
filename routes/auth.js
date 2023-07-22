d4d4c2ae39cd0ffb03ec43fcd62739d279ce2ac4
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-07-22 16:11:16 +0700

    add auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { login } from "../controller/auth.js";
-
+import { login, test } from "../controller/auth.js";
+import { verifyAdmin, verifyUser } from "../middleware/auth.js"
 const router = Router()
 
 
 router.post("/login", login)
+router.get("/", verifyUser, test)
 
 export default router
