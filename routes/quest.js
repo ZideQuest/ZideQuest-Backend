35c258e5bbc3f1ca416b2ffec6d9b3864e357a89
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-07-24 21:45:28 +0700

    quest routes

--- routes/quest.js
+++ routes/quest.js
@@ -1,13 +1,15 @@
 import { Router } from "express";;
 import { createQuest, deleteQuestById, getQuest, getQuestById, updateQuestById } from "../controller/quest.js";
-import { verifyAdmin, verifyUser } from "../middleware/auth.js";
+import { verifyCreator } from "../middleware/auth.js";
 
 const router = Router()
 
-router.post("/:locationId", verifyAdmin, createQuest)
+router.post("/:locationId", verifyCreator, createQuest)
 router.get("/", getQuest)
 router.get("/:id", getQuestById)
-router.put("/:id", verifyAdmin, updateQuestById)
-router.delete("/:id", verifyAdmin, deleteQuestById)
+router.put("/:id", verifyCreator, updateQuestById)
+router.delete("/:id", verifyCreator, deleteQuestById)
+
+
 
 export default router
