68e6f42047d8aa729929fce3391b21f886785c39
phakpoomAchalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-07-24 10:21:16 +0700

    try create locaiton

--- controller/location.js
+++ controller/location.js
@@ -1,17 +1,31 @@
+import mongoose from "mongoose";
 import Location from "../model/locaiton.js"
 import { createError } from "../util/createError.js"
 
 export const createLocation = async (req, res, next) => {
     try {
-        const { locationName, latitude, longitude, adminId } = req.body;
-        if (!(locationName && latitude && longitude && adminId)) {
+        const { locationName, latitude, longitude } = req.body;
+        // const { id } = req.user;
+        // if (!(locationName && latitude && longitude && id)) {
+        //     throw new Error("All field is required");
+        // }
+        if (!(locationName && latitude && longitude)) {
             throw new Error("All field is required");
+        }
+        try {
+            const newLocation = await Location.create({
+                locationName,
+                latitude,
+                longitude,
+                // adminId: id
+            });
+            return res.json(newLocation);
+        } catch (error) {
+            throw error;
         }
     } catch (error) {
         throw(error);
     }
-    
-    return
 }
 
 export const getAllLocation = (req, res, next) => {
