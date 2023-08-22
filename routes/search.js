1ac31f867c062bf648c6af8ccf070a4a4c27022f
RatchanonWeb <ratchanonweb46@gmail.com>
2023-08-22 20:39:29 +0700

    delete comment

--- routes/search.js
+++ routes/search.js
@@ -1,12 +1,9 @@
 import { Router } from "express";;
 import { getSearch } from "../controller/search.js";
-//import { verifyAdmin } from "../middleware/auth.js";
+
 
 const router = Router()
 
 router.get("", getSearch)
-//router.post("/", verifyAdmin, createTag)
-//router.put("/:id", verifyAdmin, updateTagById)
-//router.delete("/:id", verifyAdmin, deleteTagById)
 
 export default router
