0acb648799f38c944432da5fa7ced2b406150581
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-08-27 23:08:09 +0700

    example

--- routes/account.js
+++ routes/account.js
@@ -3,6 +3,14 @@ import { createAccount, getAccount } from "../controller/account.js";
 
 const router = Router()
 
+/**
+ * @swagger
+ * /account:
+ *   get:
+ *     summary: account
+ *     description: testing an API.
+*/
+
 router.get("/", getAccount)
 router.post("/", createAccount)
 
