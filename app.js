29232dafbe29719bcf0d1f6dd8d25f02bc4086f5
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-08-27 23:11:34 +0700

    change server

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ function createApp() {
             servers: [
                 {
                     // url: "https://3ae4-2001-fb1-1c-c64-fe34-97ff-fea7-ade2.ngrok-free.app/",
-                    url: "http://localhost:3000"
+                    url: "http://localhost:3000/api"
                 },
             ],
         },
