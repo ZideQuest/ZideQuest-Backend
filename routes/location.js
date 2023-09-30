6e8d6596acf5afa2a14e2aa45b221666e00279fa
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-09-30 17:09:27 +0700

    add middleware

--- routes/location.js
+++ routes/location.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { createLocation, getAllLocation, getLocationById, updateLocationById, deleteLocaitonById } from "../controller/location.js";
-import { verifyAdmin, verifyCreator } from "../middleware/auth.js";
+import { verify, verifyAdmin, verifyCreator } from "../middleware/auth.js";
 import { upload } from "../middleware/uploadImg.js";
 const router = Router();
 
 router.post("/", verifyCreator, upload.single('img'), createLocation);
 router.get("/", getAllLocation);
-router.get("/:id", getLocationById);
+router.get("/:id", verify, getLocationById);
 router.put("/:id", verifyCreator, upload.single('img'), updateLocationById);
 router.delete("/:id", verifyCreator, deleteLocaitonById);
 
